4f9ea49a752faa4fd246396fe9e6a51cc98d33c8
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-06 02:04:27 +0000

    Call HomeItem

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import { myData } from '../utils/data';
+import HomeItem from '../components/HomeItem';
+import WaterMark from './waterMark'
 
 const Home = () => {
   return (
-    <Layout> </Layout>
-  )
-}
+    <>
+    {/* <WaterMark /> */}
+      <Layout title="Home Page">
+        <div className="grid grid-cols-1 mx-4 gap-4 sm:gap-2 md:grid-cols-3 lg:grid-cols-3">
+          {myData.homeScreens.map((home) => (
+            <HomeItem home={home} key={home.slug} />
+          ))}
+        </div>
+      </Layout>
+    </>
+  );
+};
 
-export default Home
+export default Home;
