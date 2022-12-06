0fc3d429556895dffcd0e35b02f6c3b4983bf6a0
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-06 19:25:07 +0000

    mounted HomeItem than proping

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
-import Layout from '../components/Layout';
-import { myData } from '../utils/data';
 import HomeItem from '../components/HomeItem';
-import WaterMark from './waterMark'
+import Layout from '../components/Layout';
+import WaterMark from './waterMark';
 
 const Home = () => {
   return (
-    <>
-    {/* <WaterMark /> */}
-      <Layout title="Home Page">
-        <div className="grid grid-cols-1 mx-4 gap-4 sm:gap-2 md:grid-cols-3 lg:grid-cols-3">
-          {myData.homeScreens.map((home) => (
-            <HomeItem home={home} key={home.slug} />
-          ))}
-        </div>
-      </Layout>
-    </>
+    <Layout>
+      <div className="">
+        <HomeItem />
+      </div>
+    </Layout>
   );
 };
 
 export default Home;
+
+// <>
+// {/* <WaterMark /> */}
+//   <Layout title="Home Page">
+//     <div className="grid grid-cols-1 mx-4 gap-4 sm:gap-2 md:grid-cols-3 lg:grid-cols-3">
+//       {myData.homeScreens.map((home) => (
+//         <HomeItem home={home} key={home.slug} />
+//       ))}
+//     </div>
+//   </Layout>
+// </>
