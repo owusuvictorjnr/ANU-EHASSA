4441e5ed3e1c5d4849ba9c6a07d3d04e76b5a3cb
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-06 02:10:48 +0000

    utils data for proping

---
+++ utils/data.js
@@ -0,0 +1,170 @@
+export const myData = {
+  //   users: [
+  //     {
+  //       name: 'vitech',
+  //       email: 'vitech@gmail.com',
+  //       password: bcrypt.hashSync('123456'),
+  //       isAdmin: true,
+  //     },
+
+  //     {
+  //       name: 'Solutions',
+  //       email: 'solutions@gmail.com',
+  //       password: bcrypt.hashSync('123456'),
+  //       isAdmin: false,
+  //     },
+  // ],
+
+  // navLinks: [{}],
+
+  homeScreens: [
+    {
+      slug: 'cs',
+      name: 'computer science',
+      image: '/images/main.jpg',
+    },
+
+    {
+      slug: 'ce',
+      name: 'computer engineering',
+      image: '/images/main.jpg',
+    },
+
+    {
+      name: 'biomedical engineering',
+      image: '/images/main.jpg',
+    },
+
+    {
+      slug: 'nursing',
+      name: 'nursing',
+      image: '/images/main.jpg',
+    },
+
+    {
+      slug: 'og',
+      name: 'oil & gas',
+      image: '/images/main.jpg',
+    },
+
+    {
+      slug: 'ece',
+      name: 'electrical & communication engineering',
+      image: '/images/main.jpg',
+    },
+
+    // {
+    //   name: '',
+    //   image: '/images/main.jpg',
+    // },
+  ],
+
+  depExe: [
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'president',
+    },
+
+    {
+      vicePresident: 'computer science',
+      image: '/images/main.jpg',
+      position: 'vice president',
+    },
+
+    {
+      secretary: 'computer science',
+      image: '/images/main.jpg',
+      position: 'porter',
+    },
+
+    {
+      financialSec: 'computer science',
+      image: '/images/main.jpg',
+      position: 'secretary',
+    },
+
+    {
+      porter: 'computer science',
+      image: '/images/main.jpg',
+      position: 'financial secretary',
+    },
+
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'organizer',
+    },
+
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'patron',
+    },
+  ],
+
+  lecturers: [
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'head of department',
+    },
+
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'assistant head of department',
+    },
+
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'lecturer',
+    },
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'lecturer',
+    },
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'lecturer',
+    },
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'lecturer',
+    },
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'lecturer',
+    },
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'lecturer',
+    },
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'lecturer',
+    },
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'lecturer',
+    },
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'lecturer',
+    },
+    {
+      name: 'computer science',
+      image: '/images/main.jpg',
+      position: 'lecturer',
+    },
+  ],
+};
