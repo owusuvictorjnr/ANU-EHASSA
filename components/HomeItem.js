df9608469c0cf8e847ba6a1f9b0e1ad1ef1f485a
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-06 19:22:24 +0000

    Proping from the HomeItem and not from the index

--- components/HomeItem.js
+++ components/HomeItem.js
@@ -1,38 +1,75 @@
 import React from 'react';
 import Link from 'next/link';
-// import Image from 'next/image';
-// import { myData } from '../utils/data';
 import Layout from './Layout';
+import { myData } from '../utils/data';
 
-const HomeItem = ({ home }) => {
+const HomeItem = () => {
   return (
-    <>
-    
-      <div className="mb-5 mt-[8rem] block rounded-lg border border-gray-100 shadow-md">
-        <Link href={`/home/${home.slug}`} className="">
-          <img
-            src={home.image}
-            alt={home.name}
-            // height="100"
-            // width="100"
-            // object-Fit="cover"
-            className="rounded shadow"
-          />
-        </Link>
+    <div className="mt-[7rem]">
+      <div className="bg-yellow-600 py-2 px-4 uppercase text-3xl tracking-wide font-serif  ">
+        <h3> our departments</h3>
 
-        <div className="flex flex-col items-center justify-center p-5">
-          <Link href={`/home/${home.slug}`}>
-            <h2 className="text-lg capitalize font-serif text-black/80">
-              {home.name}
-            </h2>
-          </Link>
-          <button className="bg-amber-300 py-2 px-4 outline-none hover:bg-amber-400 active:bg-amber-500">
-            read more
-          </button>
-        </div>
+        {/* h-[calc(100vh_-_144px)] */}
       </div>
-    </>
+      <div className='bg-blue-500 h-[0.1rem] sticky top-0 w-full divide-red-500'/>
+      <div className='divide-y-yellow-600' />
+      <div
+        className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-2 mb-5 mt-[1rem]  
+      rounded-lg border border-gray-100 shadow px-4
+      "
+      >
+        {myData.homeScreens.map((home, _index) => {
+          return (
+            <div className="">
+              <Link href={`/home/${home.slug}`} className="">
+                <img
+                  src={home.image}
+                  alt={home.name}
+                  className="rounded-md   h-[20rem] w-[30rem] mb-5 mt-[3rem] block border border-gray-100 shadow-md "
+                />
+                <div
+                  key={_index.slug}
+                  className="text-2xl text-black/80 font-serif capitalize"
+                >
+                  {home.name}
+                </div>
+              </Link>
+              <button className="bg-amber-300 rounded-md py-2 px-4 outline-none hover:bg-amber-400 active:bg-amber-500 text-1xl text-black/60 font-serif font-semibold capitalize">
+                read more
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
 export default HomeItem;
+
+// <>
+
+//   <div className="mb-5 mt-[8rem] block rounded-lg border border-gray-100 shadow-md">
+//     <Link href={`/home/${home.slug}`} className="">
+//       <img
+//         src={home.image}
+//         alt={home.name}
+//         // height="100"
+//         // width="100"
+//         // object-Fit="cover"
+//         className="rounded shadow"
+//       />
+//     </Link>
+
+//     <div className="flex flex-col items-center justify-center p-5">
+//       <Link href={`/home/${home.slug}`}>
+//         <h2 className="text-lg capitalize font-serif text-black/80">
+//           {home.name}
+//         </h2>
+//       </Link>
+//       <button className="bg-amber-300 py-2 px-4 outline-none hover:bg-amber-400 active:bg-amber-500">
+//         read more
+//       </button>
+//     </div>
+//   </div>
+// </>
