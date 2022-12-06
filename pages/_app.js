c77c524fb05b1f25727bf89cdb2d618f62e92967
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-06 02:03:06 +0000

    add navbar and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+// import Layout from '../components/Layout';
+import '../styles/globals.css';
+// import WaterMark from './WaterMark';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      {/* <WaterMark /> */}
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
-
-export default MyApp
+  
+export default MyApp;
