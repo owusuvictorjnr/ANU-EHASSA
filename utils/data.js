d217ad466ae1f5955d29e190b0f0892e5db5bf7d
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-15 07:53:53 -0500

    new data

--- utils/data.js
+++ utils/data.js
@@ -19,160 +19,307 @@ export const myData = {
 
   homeScreens: [
     {
-      slug: 'cs',
-      name: 'computer science',
-      image: '/images/main.jpg',
+      slug: "cs",
+      name: "computer science",
+      image: "/images/main.jpg",
     },
 
     {
-      slug: 'ce',
-      name: 'computer engineering',
-      image: '/images/main.jpg',
+      slug: "ce",
+      name: "computer engineering",
+      image: "/images/main.jpg",
     },
 
     {
-      slug: 'bme',
-      name: 'biomedical engineering',
-      image: '/images/main.jpg',
+      slug: "bme",
+      name: "biomedical engineering",
+      image: "/images/main.jpg",
     },
 
     {
-      slug: 'nursing',
-      name: 'nursing',
-      image: '/images/main.jpg',
+      slug: "nursing",
+      name: "nursing",
+      image: "/images/main.jpg",
     },
 
     {
-      slug: 'og',
-      name: 'oil & gas',
-      image: '/images/main.jpg',
+      slug: "og",
+      name: "oil & gas",
+      image: "/images/main.jpg",
     },
 
     {
-      slug: 'ece',
-      name: 'electrical & communication engineering',
-      image: '/images/main.jpg',
+      slug: "ece",
+      name: "electrical & communication engineering",
+      image: "/images/main.jpg",
     },
-
     // {
     //   name: '',
     //   image: '/images/main.jpg',
     // },
   ],
 
-  depExe: [
+  // depExe: [
+  //   {
+  //     slug: "cs",
+  //     name: "Owusu Victor",
+  //     image: "/images/main.jpg",
+  //     position: "president",
+  //   },
+
+  //   {
+  //     name: "Michallina Appiah",
+  //     image: "/images/main.jpg",
+  //     position: "vice president",
+  //   },
+
+  //   {
+  //     name: "Nana esi",
+  //     image: "/images/main.jpg",
+  //     position: "secretary",
+  //   },
+
+  //   {
+  //     name: "benedicta menu",
+  //     image: "/images/main.jpg",
+  //     position: "financial secretary",
+  //   },
+
+  //   {
+  //     name: "odikro isaac",
+  //     image: "/images/main.jpg",
+  //     position: "porter",
+  //   },
+
+  //   {
+  //     name: "ebenezer quinoo",
+  //     image: "/images/main.jpg",
+  //     position: "organizer",
+  //   },
+
+  //   {
+  //     name: "mr raphal emmanuel kofi",
+  //     image: "/images/main.jpg",
+  //     position: "patron",
+  //   },
+
+  //   {
+  //     name: "dr theophelous oware",
+  //     image: "/images/main.jpg",
+  //     position: "h.o.d",
+  //   },
+  // ],
+
+  allData: [
     {
-      slug: 'cs',
-      name: 'Owusu Victor',
-      image: '/images/main.jpg',
-      position: 'president',
+      slug: "cs",
+      programmes: "computer science",
+      courses: [
+        "c++",
+        "networking",
+        "java",
+        "c",
+        "database management system",
+        "artificial inteligence",
+        "data science",
+        "web tech",
+        "cyber security",
+      ],
+      executives: [
+        [
+          {
+            name: "owusu victor",
+            image: "/images/main.jpg",
+            position: "president",
+          },
+          {
+            name: "michillina",
+            image: "/images/main.jpg",
+            position: "president",
+          },
+
+          {
+            name: "eben",
+            image: "/images/main.jpg",
+            position: "organizer",
+          },
+
+          {
+            name: "odikro",
+            image: "/images/main.jpg",
+            position: "portal",
+          },
+        ],
+      ],
+
+      associations: [
+        "google developer student clubs",
+        "huawei ICT academy",
+        "microsoft student academy",
+        "ingressive for good",
+        "computer science & engineering association of ghana",
+      ],
     },
 
     {
-      name: 'Michallina Appiah',
-      image: '/images/main.jpg',
-      position: 'vice president',
+      slug: "ce",
+      programmes: "computer science",
+      courses: [
+        "c++",
+        "networking",
+        "java",
+        "c",
+        "database management system",
+        "artificial inteligence",
+        "data science",
+        "web tech",
+        "cyber security",
+        "engineering maths",
+      ],
+      executives: [
+        "owusu victor",
+        "appiah michallina",
+        "nana esi",
+        "odikro",
+        "quinoo ebenezer",
+        "mr. raphal emmenuel kofi",
+        "dr. theophelous oware",
+      ],
+      associations: [
+        "google developer student clubs",
+        "huawei ICT academy",
+        "microsoft student academy",
+        "ingressive for good",
+        "computer science & engineering association of ghana",
+      ],
     },
 
     {
-      name: 'Nana esi',
-      image: '/images/main.jpg',
-      position: 'secretary',
+      slug: "bme",
+      programmes: "computer science",
+      courses: [
+        "c++",
+        "networking",
+        "java",
+        "c",
+        "database management system",
+        "artificial inteligence",
+        "data science",
+        "web tech",
+        "cyber security",
+      ],
+      executives: [
+        "owusu victor",
+        "appiah michallina",
+        "nana esi",
+        "odikro",
+        "quinoo ebenezer",
+        "mr. raphal emmenuel kofi",
+        "dr. theophelous oware",
+      ],
+      associations: [
+        "google developer student clubs",
+        "huawei ICT academy",
+        "microsoft student academy",
+        "ingressive for good",
+        "computer science & engineering association of ghana",
+      ],
     },
 
     {
-      name: 'benedicta menu',
-      image: '/images/main.jpg',
-      position: 'financial secretary',
+      slug: "nursing",
+      programmes: "computer science",
+      courses: [
+        "c++",
+        "networking",
+        "java",
+        "c",
+        "database management system",
+        "artificial inteligence",
+        "data science",
+        "web tech",
+        "cyber security",
+      ],
+      executives: [
+        "owusu victor",
+        "appiah michallina",
+        "nana esi",
+        "odikro",
+        "quinoo ebenezer",
+        "mr. raphal emmenuel kofi",
+        "dr. theophelous oware",
+      ],
+      associations: [
+        "google developer student clubs",
+        "huawei ICT academy",
+        "microsoft student academy",
+        "ingressive for good",
+        "computer science & engineering association of ghana",
+      ],
     },
 
     {
-      name: 'odikro isaac',
-      image: '/images/main.jpg',
-      position: 'porter',
+      slug: "og",
+      programmes: "computer science",
+      courses: [
+        "c++",
+        "networking",
+        "java",
+        "c",
+        "database management system",
+        "artificial inteligence",
+        "data science",
+        "web tech",
+        "cyber security",
+      ],
+      executives: [
+        "owusu victor",
+        "appiah michallina",
+        "nana esi",
+        "odikro",
+        "quinoo ebenezer",
+        "mr. raphal emmenuel kofi",
+        "dr. theophelous oware",
+      ],
+      associations: [
+        "google developer student clubs",
+        "huawei ICT academy",
+        "microsoft student academy",
+        "ingressive for good",
+        "computer science & engineering association of ghana",
+      ],
     },
 
     {
-      name: 'ebenezer quinoo',
-      image: '/images/main.jpg',
-      position: 'organizer',
-    },
-
-    {
-      name: 'mr raphal emmanuel kofi',
-      image: '/images/main.jpg',
-      position: 'patron',
-    },
-
-    {
-      name: 'dr theophelous oware',
-      image: '/images/main.jpg',
-      position: 'h.o.d',
-    },
-  ],
-
-  lecturers: [
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'head of department',
-    },
-
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'assistant head of department',
-    },
-
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'lecturer',
-    },
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'lecturer',
-    },
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'lecturer',
-    },
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'lecturer',
-    },
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'lecturer',
-    },
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'lecturer',
-    },
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'lecturer',
-    },
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'lecturer',
-    },
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'lecturer',
-    },
-    {
-      name: 'computer science',
-      image: '/images/main.jpg',
-      position: 'lecturer',
+      slug: "ece",
+      programmes: "computer science",
+      courses: [
+        "c++",
+        "networking",
+        "java",
+        "c",
+        "database management system",
+        "artificial inteligence",
+        "data science",
+        "web tech",
+        "cyber security",
+      ],
+      executives: [
+        "owusu victor",
+        "appiah michallina",
+        "nana esi",
+        "odikro",
+        "quinoo ebenezer",
+        "mr. raphal emmenuel kofi",
+        "dr. theophelous oware",
+      ],
+      associations: [
+        "google developer student clubs",
+        "huawei ICT academy",
+        "microsoft student academy",
+        "ingressive for good",
+        "computer science & engineering association of ghana",
+      ],
     },
   ],
 };
