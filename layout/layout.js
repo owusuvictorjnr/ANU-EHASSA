053cffadc180023bd6ed7f5cb7b31c54f4769820
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-17 14:36:54 +0000

    Featured: Auth layout

---
+++ layout/layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Login from '../pages/login';
+
+const LoginsLayout = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default LoginsLayout;
