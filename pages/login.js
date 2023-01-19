746aca7995ee66a1d10f37075c523a1733548653
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-19 20:02:25 +0000

    Featured: Login page

--- pages/login.js
+++ pages/login.js
@@ -1,23 +1,12 @@
 import React from 'react';
+import LoginsLayout from '../components/LoginsLayout';
 
 const Login = () => {
   return (
     <>
-      <div>
-        <div>
-          <section className="w-3/4 mx-auto flex flex-col gap-10 ">
-            <div className="">
-              <h1 className="text-gray-800 text-4xl font-bold uppercase">
-                anu ehassa
-              </h1>
-
-              <p className="w-3/4 mx-auto text-gray-400 ">
-                Aliquip nisi dolore enim reprehenderit elit consequat.
-              </p>
-            </div>
-          </section>
-        </div>
-      </div>
+      <LoginsLayout>
+        <div className="">log</div>
+      </LoginsLayout>
     </>
   );
 };
