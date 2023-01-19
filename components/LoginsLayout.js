72b5bfc41bba2b23d181cd00f4488ff7b8a488d0
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-19 19:53:50 +0000

    Featured: Login layout for

---
+++ components/LoginsLayout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Layout from './Layout';
+import styles from '../styles/LoginsLayout.module.css';
+
+const LoginsLayout = ({ children }) => {
+  return (
+    <Layout>
+      <div className="flex h-screen bg-blue-200">
+        <div className="m-auto bg-yellow-200 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
+          <div className={styles.imgStyle}>
+            <div className={styles.logoanimi}></div>
+            <div className={styles.cessa}></div>
+            <div className={styles.anumain}></div>
+          </div>
+
+          <div className="right flex flex-col justify-evenly bg-gray-50">
+            <div className="text-center py-10 ">{children}</div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default LoginsLayout;
