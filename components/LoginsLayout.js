732a82e3cb92201b590503088e0edc87ee311b14
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-20 07:09:38 +0000

    Featured: Login page background color changed

--- components/LoginsLayout.js
+++ components/LoginsLayout.js
@@ -13,7 +13,7 @@ const LoginsLayout = ({ children }) => {
             <div className={styles.anumain}></div>
           </div>
 
-          <div className="right flex flex-col justify-evenly bg-gray-50">
+          <div className="right flex flex-col justify-evenly">
             <div className="text-center py-10 ">{children}</div>
           </div>
         </div>
