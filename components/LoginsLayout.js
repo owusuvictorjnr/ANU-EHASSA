3c6727a97f49b65b232c925b7633d1b1252998a9
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-21 16:48:00 +0000

    Featured: Given min-height

--- components/LoginsLayout.js
+++ components/LoginsLayout.js
@@ -6,7 +6,7 @@ const LoginsLayout = ({ children }) => {
   return (
     <Layout>
       <div className="flex h-screen bg-blue-200">
-        <div className="m-auto bg-yellow-200 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
+        <div className="m-auto bg-yellow-200 rounded-md w-3/5 min-h-3/4 grid lg:grid-cols-2">
           <div className={styles.imgStyle}>
             <div className={styles.logoanimi}></div>
             <div className={styles.cessa}></div>
