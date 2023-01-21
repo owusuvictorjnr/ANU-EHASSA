f371856793f989ebdee45988b41697174562a722
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-21 16:49:29 +0000

    Deleted: watermark

--- pages/index.js
+++ pages/index.js
@@ -12,15 +12,9 @@ const Home = () => {
   );
 };
 
+
+
+
+
 export default Home;
 
-// <>
-// {/* <WaterMark /> */}
-//   <Layout title="Home Page">
-//     <div className="grid grid-cols-1 mx-4 gap-4 sm:gap-2 md:grid-cols-3 lg:grid-cols-3">
-//       {myData.homeScreens.map((home) => (
-//         <HomeItem home={home} key={home.slug} />
-//       ))}
-//     </div>
-//   </Layout>
-// </>
