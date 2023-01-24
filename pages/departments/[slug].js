1c8b4eb0a758fdf102d67d3c68a6310a9a4fcf14
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-24 10:05:25 +0000

    Featured: Add Programme to subheader and create association section

--- pages/departments/[slug].js
+++ pages/departments/[slug].js
@@ -18,13 +18,6 @@ const HomeScreen = ({ department }) => {
   const [data, setData] = useState(jsonData);
   const { query } = useRouter();
   const { slug } = query;
-  // const myData = data[slug];
-  // const departments = myData.homeScreens.find((x) => x.slug === slug);
-  // if (!departments) {
-  //   return <div>Department Not Found</div>;
-  // }
-
-  // props for departmental executives
 
   useEffect(() => {
     const dept = jsonData[slug];
@@ -35,48 +28,61 @@ const HomeScreen = ({ department }) => {
 
   return (
     <Layout title={department.name}>
-      <div className="mt-[7rem]">
-        <SubHeaders label="about " />
+      <div>
+        <div className="mt-[7rem]">
+          <SubHeaders label="about " programme={department.programmes} />
 
-        <div className="bg-red-600 h-[0.3rem] sticky top-0 w-full divide-red-500" />
-        <div className="bg-blue-500 min-h-[20rem]">
-          TODO: About the department.
+          <div className="bg-red-600 h-[0.3rem] sticky top-0 w-full divide-red-500" />
+          <div className="bg-blue-500 min-h-[20rem]">
+            TODO: About the department.
+          </div>
         </div>
-      </div>
 
-      <div className="">
-        <SubHeaders label="executives" className="sticky top-52" />
+        <div className="">
+          <SubHeaders label="executives" className="sticky top-52" />
 
-        <div className="bg-red-600 h-[0.2rem]  w-full sh-underline1" />
-        <div className="grid  grid-cols-1 lg:grid-cols-5 md:grid-cols-2 mb-5 rounded-lg border border-gray-100 shadow md:pl-[9rem] pr-20 gap-5">
-          {department.executives.map((executive, _index) => (
-            <div className="">
+          <div className="bg-red-600 h-[0.2rem]  w-full sh-underline1" />
+          <div className="grid  grid-cols-1 lg:grid-cols-4 px-12 md:grid-cols-2 mb-5 rounded-lg border border-gray-100 shadow md:pl-[9rem] pr-20 gap-5">
+            {department.executives.map((executive, _index) => (
               <div className="">
-                <img
-                  src={executive.image}
-                  alt={executive.name}
-                  className="rounded-md h-[10rem] w-[15rem] mb-5 mt-[3rem] block border border-gray-100 shadow-md z-100"
-                />
-                <div
-                  key={_index.slug}
-                  className="text-2xl text-black/80 font-serif capitalize"
-                >
-                  {executive.name}
-                </div>
-                <div className="text-1xl capitalize font-serif text-black/70 font-semibold pb-5">
-                  <h1> {executive.position}</h1>
+                <div className="">
+                  <img
+                    src={executive.image}
+                    alt={executive.name}
+                    className="rounded-md h-[10rem] w-[15rem] mb-5 mt-[3rem] block border border-gray-100 shadow-md z-100"
+                  />
+                  <div
+                    key={_index.slug}
+                    className="text-2xl text-black/80 capitalize"
+                  >
+                    {executive.name}
+                  </div>
+                  <div className="text-1xl capitalize font-serif text-black/70 font-semibold pb-5">
+                    <h1> {executive.position}</h1>
+                  </div>
                 </div>
               </div>
+            ))}
+          </div>
+          <div className="">
+            <div className="md:mt-[2rem]">
+              <SubHeaders label="courses" />
+              <div className="bg-red-600 h-[0.2rem]  w-full sh-underline1" />
             </div>
-          ))}
+            <div className="min-h-[20rem] bg-red-200 mb-5  p-4 order-4 ">
+              <h1 className="">{department.courses}</h1>
+            </div>
+          </div>
         </div>
+
+        {/*Associations*/}
         <div className="">
           <div className="md:mt-[2rem]">
-            <SubHeaders label="courses" />
+            <SubHeaders label="associations" />
             <div className="bg-red-600 h-[0.2rem]  w-full sh-underline1" />
           </div>
           <div className="min-h-[20rem] bg-red-200 mb-5  p-4 order-4 ">
-            <h1 className="">{department.courses}</h1>
+            <h1 className="">{department.associations}</h1>
           </div>
         </div>
       </div>
