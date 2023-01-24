fcf366205b0ca9749fbc0f5121f4369d93db343f
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-24 10:02:04 +0000

    Feature: Added Programme

--- components/SubHeaders.js
+++ components/SubHeaders.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const SubHeaders = ({ label }) => {
+const SubHeaders = ({ label, programme }) => {
   return (
     <div>
       <div className="bg-yellow-600 mt-[2rem] py-2 px-4 uppercase text-3xl tracking-wide font-serif ">
         {label}
+        {programme}
       </div>
     </div>
   );
