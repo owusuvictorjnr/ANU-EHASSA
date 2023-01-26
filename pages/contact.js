474c0b2b6d12c15df531291d570fd602679128ff
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-26 12:34:35 +0000

    Feature: Contact header

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,18 @@
+import Layout from "../components/Layout";
+
+
+
 const Contact = () => {
-  return <div>Contact us</div>;
+  return (
+    <Layout title={`contact`}>
+      <div className=" flex flex-col h-screen mt-[7rem]">
+        <div className="flex  mx-auto top-[7rem] sticky">
+          <h1 className="bg-blue-900 w-screen justify-center h-10 flex items-center mx-auto text-white uppercase text-4xl py-2">
+            contact ehassa
+          </h1>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 export default Contact;
