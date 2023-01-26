cddc0009b83b361a87f2966cb4b46696af2e112b
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-26 12:34:03 +0000

    Feature: About banner

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 const AboutPage = () => {
-  return <div>AboutScreen</div>;
+  return (
+    <Layout>
+      <div className=" flex flex-col h-screen mt-[7rem]">
+        <div className="flex  mx-auto top-[7rem] sticky">
+          <h1 className="bg-blue-900 w-screen justify-center h-10 flex items-center mx-auto text-white uppercase text-4xl py-2">
+            about ehassa
+          </h1>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default AboutPage;
