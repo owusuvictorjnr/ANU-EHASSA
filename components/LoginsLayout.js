54d516591cf0c8a69741284c7828b04a5ac1fd11
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-26 12:33:19 +0000

    Feature: background color change

--- components/LoginsLayout.js
+++ components/LoginsLayout.js
@@ -6,7 +6,7 @@ const LoginsLayout = ({ children }) => {
   return (
     <Layout>
       <div className="flex h-screen bg-gradient-to-br from-yellow-300 to-red-100 pt-36">
-        <div className="m-auto bg-blue-400 rounded-md w-3/5 min-h-3/4 grid lg:grid-cols-2">
+        <div className="m-auto bg-blue-900 rounded-md w-3/5 min-h-3/4 grid lg:grid-cols-2 ">
           <div className={styles.imgStyle}>
             <div className={styles.logoanimi}></div>
             <div className={styles.cessa}></div>
