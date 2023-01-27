f526beb58e0f4cbf2c2eaab3943ee9d0c507b6e2
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-27 08:36:44 +0000

    Feature: Student dashboard created

---
+++ pages/students/[student].js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+const Student = () => {
+  return (
+    <section title={'Student'}>
+      <div>student homepage</div>
+    </section>
+  );
+};
+
+export default Student;
