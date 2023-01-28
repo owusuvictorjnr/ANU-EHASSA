72b00124718d0e009d1b684c595dacb069934b3d
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-28 14:06:39 +0000

    Featured: Dept Executive bg change

--- pages/departments/[slug].js
+++ pages/departments/[slug].js
@@ -42,10 +42,10 @@ const HomeScreen = ({ department }) => {
           <SubHeaders label="executives" className="sticky top-52" />
 
           <div className="bg-red-600 h-[0.2rem]  w-full sh-underline1" />
-          <div className="grid  grid-cols-1 lg:grid-cols-4 px-12 md:grid-cols-2 mb-5 rounded-lg border border-gray-100 shadow md:pl-[9rem] pr-20 gap-5">
+          <div className="grid  grid-cols-1  lg:grid-cols-4 px-12 md:grid-cols-2 mb-5 rounded-lg border border-gray-100 shadow md:pl-[9rem] pr-20 gap-5 bg-[#023047]">
             {department.executives.map((executive, _index) => (
               <div className="">
-                <div className="">
+                <div className="pl-[3rem]">
                   <img
                     src={executive.image}
                     alt={executive.name}
@@ -53,11 +53,11 @@ const HomeScreen = ({ department }) => {
                   />
                   <div
                     key={_index.slug}
-                    className="text-2xl text-black/80 capitalize"
+                    className="text-2xl text-white capitalize"
                   >
                     <div className="">{executive.name}</div>
                   </div>
-                  <div className="text-1xl capitalize font-serif text-black/70 font-semibold pb-5">
+                  <div className="text-1xl capitalize text-[#ffb703] font-semibold pb-5">
                     <h1> {executive.position}</h1>
                   </div>
                 </div>
