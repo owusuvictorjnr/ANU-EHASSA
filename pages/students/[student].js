12c918b12f4c532a38532844a6211df03b2923bb
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-28 12:12:54 +0000

    Featured:Create Navbar

--- pages/students/[student].js
+++ pages/students/[student].js
@@ -1,10 +1,40 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import React from 'react';
-import Layout from '../../components/Layout';
 
 const Student = () => {
   return (
-    <section title={'Student'}>
-      <div>student homepage</div>
+    <section>
+      <Head>
+        <title>Student</title>
+      </Head>
+      <div className="bg-[#fcbf49] h-screen mx-auto ">
+        <div className="bg-[#1d3557] h-40 shadow-md border-b">
+          <div className="flex justify-center ">
+            <Image
+              src="/logo/ehassa.jpg"
+              alt="logo"
+              height={100}
+              width="100"
+              className="rounded-full pt-10"
+            />
+          </div>
+
+          <nav className="flex justify-end items-end">
+            <ul className="mx-auto px-5 py-2 flex ">
+              <li>home</li>
+
+              <li>register</li>
+
+              <li>read projects</li>
+
+              <li>read lecture notes</li>
+
+              <li>search</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </section>
   );
 };
