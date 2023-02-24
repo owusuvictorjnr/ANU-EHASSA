2b9dbe64a2b491113efa384c276cfe953d2792e8
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-24 11:36:41 +0000

    Fix: Text size

--- components/SubHeaders.js
+++ components/SubHeaders.js
@@ -3,7 +3,7 @@ import React from "react";
 const SubHeaders = ({ label, programme }) => {
   return (
     <div>
-      <div className="bg-yellow-600 mt-[2rem] py-2 px-4 uppercase text-3xl tracking-wide font-serif ">
+      <div className="bg-yellow-300 py-2 px-4 uppercase">
         {label}
         {programme}
       </div>
