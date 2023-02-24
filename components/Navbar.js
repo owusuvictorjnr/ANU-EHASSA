124b9b13d9caa3803d3092acc36785b0fbba5e63
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-24 11:35:58 +0000

    Fix: Responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -42,62 +42,57 @@ const Navbar = ({ department }) => {
 
   return (
     <div>
-      <div className="shadow-xl h-[7rem]  fixed top-0 z-20 left-0 right-0  bg-yellow-500  flex justify-evenly capitalize text-1xl">
+      <div className="shadow-md h-[4rem] fixed top-0 z-20 left-0 right-0  bg-yellow-100 flex justify-between">
         <div className="flex">
           <Link href="/">
             <Image
-              src="/logo/ehassa.jpg"
+              src="/logo/ehassa.png"
               alt="cessa"
-              width="80"
-              height="80"
-              className="bg-yellow-500 rounded-full  py-1 mt-3"
+              width="300"
+              height="300"
+              className="rounded-full h-16 w-16 py-1 mx-5 mt-3 pb-4"
             />
           </Link>
-          <Link href="/" className="cursor-poniter">
-            <h2 className="mx-3  py-8 uppercase text-3xl text-blue-900 font-serif flex justify-start ">
-              ehassa
-            </h2>
-          </Link>
+          <div>
+            <Link href="/" className="cursor-poniter">
+              <h2 className="py-5 uppercase text-sm md:text-xl lg:text-3xl text-blue-900">
+                school of engineering
+              </h2>
+            </Link>
+          </div>
         </div>
 
-        {/*navnar*/}
+        {/*navbar*/}
         <div className="">
-          <nav className="">
-            <div className=" mx-5">
-              <div className=" pt-8  text-2xl text-blue-900 flex-auto justify-end item-center">
-                <ul className="space-x-5 mb-4  flex justify-end items-end lg:justify-end hidden md:flex">
-                  <Link href="/" className="cursor-pointer">
-                    <li className="hover:bg-white py-1 px-2 rounded hover:underline duration-500">
-                      home
-                    </li>
-                  </Link>
-                  <Link href="/about" className="cursor-pointer">
-                    <li className="hover:bg-white py-1 px-2 rounded hover:underline duration-500">
-                      about
-                    </li>
-                  </Link>
-                  <Link href="/contact">
-                    <li className="hover:bg-white py-1 px-2 rounded hover:underline duration-500">
-                      contact
-                    </li>
-                  </Link>
+          <nav className="py-4 mx-5 capitalize text-sm text-blue-900 space-x-10 flex justify-between md:text-xl lg:text-3xl ">
+            <ul className=" hidden md:flex lg:gap-10 md:gap-5">
+              <Link href="/" className="cursor-pointer">
+                <li className="hover:bg-white py-1 px-2 rounded hover:underline duration-500">
+                  <h1 className="">home</h1>
+                </li>
+              </Link>
+              <Link href="/about" className="cursor-pointer">
+                <li className="hover:bg-white py-1 px-2 rounded hover:underline duration-500">
+                  about
+                </li>
+              </Link>
+              <Link href="/contact">
+                <li className="hover:bg-white py-1 px-2 rounded hover:underline duration-500">
+                  contact
+                </li>
+              </Link>
 
-                  <Link href="/login" className="cursor-pointer">
-                    <li className="hover:bg-white py-1 px-2 rounded hover:underline duration-500">
-                      sign in
-                    </li>
-                  </Link>
-                </ul>
-              </div>
-            </div>
+              <Link href="/login" className="cursor-pointer">
+                <li className="hover:bg-white py-1 px-2 rounded hover:underline duration-500">
+                  sign in
+                </li>
+              </Link>
+            </ul>
           </nav>
 
           {/*side navbar */}
-          <div
-            onClick={handleNav}
-            className="flex items-center cursor-pointer md:hidden"
-          >
-            <AiOutlineMenu size={25} color={'#023e8a'} />
+          <div onClick={handleNav} className="flex cursor-pointer md:hidden">
+            <AiOutlineMenu size={25} color={'#023e8a'} className="" />
           </div>
           <div
             className={
@@ -124,34 +119,33 @@ const Navbar = ({ department }) => {
                     />
 
                     <div className="">
-                      <p className="text-3xl text-yellow-600 uppercase">
-                        {' '}
-                        ehassa
+                      <p className="text-sm text-yellow-600 uppercase">
+                        schoolof engineering
                       </p>
                     </div>
                   </div>
 
                   <div
                     onClick={handleNav}
-                    className="rounded-full shadow-lg shadow-white p-3 cursor-pointer"
+                    className="rounded-full shadow-md shadow-white p-2 cursor-pointer"
                   >
                     <AiOutlineClose
-                      size={40}
+                      size={25}
                       color={'#e63946'}
-                      className="hover:bg-white rounded-full py-2"
+                      className="hover:bg-white rounded-full py-1"
                     />
                   </div>
                 </div>
 
                 <div className="border-b border-gray-300 my-4">
-                  <h2 className="w-[85%] md:w-[90%] py-4 text-yellow-600 ">
+                  <h2 className="flex justify-center container mx-auto w-[85%] md:w-[90%] py-4 text-yellow-600 ">
                     all nations university school of engineering
                   </h2>
                 </div>
               </div>
               <div className="py-4 flex flex-col text-yellow-600">
-                <ul className="uppercase">
-                  <Link href="/" className="cursor-pointer">
+                <ul className="uppercase text-sm">
+                  <Link href="/" className="cursor-pointer ">
                     <li className="hover:bg-white py-4 px-2 rounded-md hover:underline duration-500">
                       home
                     </li>
@@ -207,10 +201,6 @@ const Navbar = ({ department }) => {
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center items-center">
-          {/* {department.logo}*/}
-          logo
         </div>
       </div>
     </div>
