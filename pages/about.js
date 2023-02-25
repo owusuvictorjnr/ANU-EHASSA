c84f263890183e0076021a1044a4cb85ce4a120f
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-25 22:25:50 +0000

    Fix: change title

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Layout from '../components/Layout';
+import Navbar from '../components/Navbar';
 
 const AboutPage = () => {
   return (
-    <Layout>
-      <div className=" flex flex-col h-screen mt-[7rem]">
-        <div className="flex  mx-auto top-[7rem] sticky">
-          <h1 className="bg-blue-900 w-screen justify-center h-10 flex items-center mx-auto text-white uppercase text-4xl py-2">
-            about ehassa
-          </h1>
+    <>
+      <Layout>
+        <Navbar />
+        <div className=" flex flex-col h-screen">
+          <div className="flex mx-auto top-[4rem] fixed">
+            <h1 className="bg-blue-900 w-screen justify-center h-10 flex items-center mx-auto ">
+              <p className="text-white uppercase text-sm md:text-xl lg:text-2xl">
+                about school of engineering and allied science
+              </p>
+            </h1>
+          </div>
         </div>
-      </div>
-    </Layout>
+        <Footer />
+      </Layout>
+    </>
   );
 };
 
