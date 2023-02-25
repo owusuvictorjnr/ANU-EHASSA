8f3d45b4ad00d4de5d7bf7a037b8cacccd9dad6a
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-25 22:24:28 +0000

    Fix: delete commented codes

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,9 @@
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
-// import Layout from '../components/Layout';
 import '../styles/globals.css';
-// import WaterMark from './WaterMark';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <Navbar />*/}
-      {/* <WaterMark /> */}
       <Component {...pageProps} />
-      {/*<Footer />*/}
     </>
   );
 }
