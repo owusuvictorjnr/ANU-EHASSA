f5605420b0c301ff2aedf921bf003b67886fbdda
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-25 22:26:45 +0000

    Fix: change title and add navbar and footer

--- pages/contact.js
+++ pages/contact.js
@@ -1,18 +1,24 @@
-import Layout from "../components/Layout";
-
-
+import Footer from '../components/Footer';
+import Layout from '../components/Layout';
+import Navbar from '../components/Navbar';
 
 const Contact = () => {
   return (
-    <Layout title={`contact`}>
-      <div className=" flex flex-col h-screen mt-[7rem]">
-        <div className="flex  mx-auto top-[7rem] sticky">
-          <h1 className="bg-blue-900 w-screen justify-center h-10 flex items-center mx-auto text-white uppercase text-4xl py-2">
-            contact ehassa
-          </h1>
+    <>
+      <Layout title={`contact`}>
+        <Navbar />
+        <div className=" flex flex-col h-screen mt-[7rem]">
+          <div className="flex mx-auto top-[4rem] fixed">
+            <h1 className="bg-blue-900 w-screen justify-center h-10 flex items-center mx-auto">
+              <p className="text-white uppercase text-sm md:text-xl lg:text-2xl">
+                contact school of engineering and allied science
+              </p>
+            </h1>
+          </div>
         </div>
-      </div>
-    </Layout>
+        <Footer />
+      </Layout>
+    </>
   );
 };
 export default Contact;
